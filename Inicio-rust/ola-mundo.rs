ab436c038bdd7c5b7e5aa38a7f74d9fde51c1798
Arlyjr <arly.lcj@gmail.com>
2023-02-02 03:53:02 -0300

    first commit

---
+++ Inicio-rust/ola-mundo.rs
@@ -0,0 +1,6 @@
+fn main() {
+    //print!("Hello World, Olá mundo desvendando linguagem Rust, essa é uma forma de print");
+    // ou exemplificando com outra forma de imprimir
+    println!("Hello World, Olá mundo desvendando linguagem Rust com outra forma de println pula linha.");
+    
+}
---
+++ praticando-variaveis/variavel.rs
@@ -0,0 +1,13 @@
+//REGRA 1 COMEÇAR COM LETRA MINUSCULA
+//REGRA 2 TERMINAR COM LETRAS MINUSCULA OU NUMEROS
+//OBS: UNDERSCORE _ PODE SER USADO ENTRE LETRA E NUMEROS
+
+fn main(){
+let msg = "olá mundo!!";
+//let msg1 = "hello mundo!";
+//let Msg = "hello world!!";
+//let 1msg = "olá hello mundo world!!";
+//let m_1_s_2_g = "olá mundo!!";
+
+println!("Sua mensagem é: {}  ", msg);
+}
