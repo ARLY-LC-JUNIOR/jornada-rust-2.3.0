b7be51be23e04c0b72a23c53326d85053d3b30b1
Arlyjr <arly.lcj@gmail.com>
2023-02-03 00:33:49 -0300

    Tipos

---
+++ 5-tipos-dados/tipo.rs
@@ -0,0 +1,14 @@
+fn main(){
+//Os principais tipos de dados em Rust
+
+let tipo1 = 'c';
+//let tipo2 = "Linguagem Rust";
+//let tipo3 =  1234;
+//let tipo4 =  true;
+//let tipo5 =  3.14;
+    println!("Dado do tipo char é : {}", tipo1 );
+    //println!(Dado do tipo string é : {}, tipo2 );
+    // println!(Dado do tipo int é : {}, tipo3 );
+    //println!(Dado do tipo bolean é : {}, tipo4 );
+    //println!(Dado do tipo float é : {}, tipo5 );
+}
