8be330f3339e130e7a60acab19a9edfb811999d2
Arlyjr <arly.lcj@gmail.com>
2023-02-03 00:11:10 -0300

    atualizando cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-0d435cf4115f8aef.d
@@ -0,0 +1,5 @@
+c:\jornada-rust-2.3.0\hello_cargo\target\debug\deps\hello_cargo-0d435cf4115f8aef.rmeta: src\main.rs
+
+c:\jornada-rust-2.3.0\hello_cargo\target\debug\deps\hello_cargo-0d435cf4115f8aef.d: src\main.rs
+
+src\main.rs:
